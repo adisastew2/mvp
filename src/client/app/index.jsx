4785aa59ae7615257b415b1fa72dd658c9637e69
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-08-06 12:32:42 -0700

    add constructor and send func with axios post

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import {render} from 'react-dom';
+import axios from 'axios';
 
 import Form from './components/form.jsx';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: []
+    };
+    
+  }
+
+  sendStuff(name, msg){
+    axios.post('/msgs', {
+      name: name,
+      msg: msg
+    })
+    .then((res) => {
+      console.log('FROM POST: ', res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
   render() {
     return(
       <div>
-        <Form />
+        <Form submit={this.sendStuff}/>
       </div>
     ) 
   }
