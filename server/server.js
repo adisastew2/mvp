43e0a8c630a07e18da849946cb046f894ff36896
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-08-06 12:34:58 -0700

    add POST route

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser')
+let app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(__dirname + '/../src/client/public'));
 
 app.get('/', (req, res)=>{
   res.send('What it Dew');
+});
+
+app.post('/msgs', (req, res)=>{
+  console.log('REQ FROM ROUTE: ', req);
 });
 
 app.listen(3000, () => {
