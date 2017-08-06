494cc242580d8b43132c891903475c6798b19aff
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-08-05 18:01:22 -0700

    create form component

---
+++ src/client/app/components/form.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Form(props) {
+  return (
+    <div>
+      <input placeholder="name"/>
+      <input placeholder="message"/>
+      <button>Submit</button>
+    </div>
+  );
+}
+
+export default Form;
