029f91d5a4623327027da448ccd1505bc0144ab5
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-08-05 18:01:45 -0700

    place form in app comp

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+import Form from './components/form.jsx';
+
 class App extends React.Component {
   render() {
-    return <div>Ayy</div>
+    return(
+      <div>
+        <Form />
+      </div>
+    ) 
   }
 }
 render(<App/>, document.getElementById('app'));
