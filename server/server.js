840112faad719b3dbe15cf97d7546389d1694fdd
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-08-07 04:47:53 -0700

    GET route for msgs from db

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 
 const save = require('../DB/index').saveNote;
+const getMsgs = require('../DB/index').getMsgs;
 let app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -10,6 +11,16 @@ app.use(express.static(__dirname + '/../src/client/public'));
 
 app.get('/', (req, res) => {
   res.send('What it Dew');
+});
+
+app.get('/msgs', function (req, res) {
+  getMsgs((err, data) => {
+    if(err){
+      res.send(501, err);
+    } 
+    res.send(data);
+    
+  });
 });
 
 app.post('/msgs', (req, res) => {
