217cc14c8a84df95d89441451c0792abfaba9a3e
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-08-07 04:20:30 -0700

    jquery POST reaches server but errors

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import axios from 'axios';
+import $ from 'jquery';
 
 import Form from './components/form.jsx';
 
@@ -14,15 +15,33 @@ class App extends React.Component {
   }
 
   sendStuff(name, msg){
-    axios.post('/msgs', {
-      name: name,
-      msg: msg
-    })
-    .then((res) => {
-      console.log('FROM POST: ', res);
-    })
-    .catch((err) => {
-      console.log(err);
+    console.log('SENDSTUFF CALLED');
+    console.log('SENDSTUFF name: ', name);
+    console.log('SENDSTUFF msg: ', msg);
+
+    // server gets empty obj
+    // axios.post('/msgs', {
+    //   name: name,
+    //   msg: msg
+    // })
+    // .then((res) => {
+    //   console.log('FROM POST: ');
+    // })
+    // .catch((err) => {
+    //   console.log(err);
+    // });
+
+    // server gets values but does not console.log 
+     $.ajax({
+      type: "POST",
+      url: "/msgs",
+      data: {name, msg},
+      success: function(data){
+        console.log('POST SUCCESS!!!');
+      },
+      error: function(err){
+        console.log('FUCK', err);
+      }
     });
   }
 
