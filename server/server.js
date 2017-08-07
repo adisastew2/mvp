6f590ec3cb6e9c1740e55835d177061dc9e4c1be
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-08-07 04:19:47 -0700

    call db save func in post route

--- server/server.js
+++ server/server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+
+const save = require('../DB/index').saveNote;
 let app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(__dirname + '/../src/client/public'));
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
   res.send('What it Dew');
 });
 
-app.post('/msgs', (req, res)=>{
-  console.log('REQ FROM ROUTE: ', req);
+app.post('/msgs', (req, res) => {
+  // console.log('FROM ROUTE: ', req.body.msg);
+  save(req.body.name, req.body.msg);
 });
 
 app.listen(3000, () => {
