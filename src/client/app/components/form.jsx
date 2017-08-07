1890c7e0fd24198986dc47c6d172505247ce75fe
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-08-06 17:31:23 -0700

    add onChange funct to form that change state with input

--- src/client/app/components/form.jsx
+++ src/client/app/components/form.jsx
@@ -13,12 +13,29 @@ import React from 'react';
 class Form  extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      name: '',
+      msg: ''
+    };
   }
+
+  onChangeName (e) {
+    this.setState({
+      name: e.target.value 
+    });
+  }
+
+   onChangeMsg (e) {
+    this.setState({
+      msg: e.target.value 
+    });
+  }
+
   render(){
     return (
     <div>
-      <input placeholder="name"/>
-      <input placeholder="message"/>
+      <input onChange={this.onChangeName.bind(this)} placeholder="name"/>
+      <input onChange={this.onChangeMsg.bind(this)}  placeholder="message"/>
       <button onClick={this.props.submit}>Submit</button>
       <button>Show Messages</button>
     </div>
